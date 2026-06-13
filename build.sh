ff4ee57dafc602428f960eb011cffdf26535b5dc
dhevesh-tai <dhevesharun@gmail.com>
2026-06-13 21:37:02 +0530

    c
    c

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Installing Flutter..."
 if [ ! -d "$HOME/flutter" ]; then
-  git clone https://github.com/flutter/flutter.git $HOME/flutter --depth 1 -b 3.24.0
+  git clone https://github.com/flutter/flutter.git $HOME/flutter --depth 1 -b 3.44.2
 fi
 export PATH="$PATH:$HOME/flutter/bin"
 sed -i 's/if \[ "$(id -u)" -eq 0 \]/if false/' $HOME/flutter/bin/flutter
